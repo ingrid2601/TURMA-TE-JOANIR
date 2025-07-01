c8814d4f783c8c97fb46e9159911a2c0221006a2
ingrid2601 <159648748+ingrid2601@users.noreply.github.com>
2025-06-30 22:30:52 -0300

    .

--- imagem/script.js
+++
@@ -1,8 +0,0 @@
-function inserirNome(){
-   let nomeUsuario = prompt("Qual o seu nome");
-   let elemento = document.querySelector("#nome-usuario");
-   console.log(elemento);
-   elemento.textContent = nomeUsuario;
-}
-
-inserirNome();
