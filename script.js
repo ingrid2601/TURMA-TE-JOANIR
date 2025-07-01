544bca4a58ea0ff45a6e62f6e82db3f915347f14
ingrid2601 <159648748+ingrid2601@users.noreply.github.com>
2025-07-01 14:40:49 -0300

    .

--- script.js
+++ script.js
@@ -11,3 +11,9 @@ let linguagens = ["JavaScript", "Python","C++"]
 console.log (linguagens [0])
 console.log (linguagens [1])
 console.log (linguagens [2])
+
+
+const lista = document.querySelector ("#lista");
+lista.textContent = linguagens [0];
+lista.textContent = linguagens [1];
+lista.textContent = linguagens [2];
