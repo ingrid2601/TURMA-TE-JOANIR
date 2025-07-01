ce60555e2348e85371436a5c809e1fef17db5399
ingrid2601 <159648748+ingrid2601@users.noreply.github.com>
2025-07-01 15:25:30 -0300

    .

--- script.js
+++ script.js
@@ -16,4 +16,3 @@ console.log (linguagens [2])
 const lista = document.querySelector ("#lista");
 lista.textContent = linguagens [0];
 lista.textContent = linguagens [1];
-lista.textContent = linguagens [2];
